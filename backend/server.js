166db556ea1871a86a53235ed3ff98deb528d411
AndreiPopescu21 <70022581+AndreiPopescu21@users.noreply.github.com>
2021-01-30 01:40:45 +0200

    Connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose=require('mongoose');
 
 const PORT = process.env.PORT || 3000;
+
+// Connect to DB
+mongoose.connect('mongodb+srv://Admin:Password1@projecthappy.y224t.mongodb.net/ProjectHappy?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
