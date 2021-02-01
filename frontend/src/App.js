abe6135632f5e0dce8883fc49f1016be430a9449
AndreiPopescu21 <70022581+AndreiPopescu21@users.noreply.github.com>
2021-02-01 21:41:17 +0200

    Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 // React router
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Navbar from './Components/navbar'
 
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar/>
         <Switch>
           <Route exact path="/">
             <h1>Project happy</h1>
---
+++ frontend/src/Components/navbar.js
@@ -0,0 +1,41 @@
+import { Link, NavLink } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import logo from '../images/logo.svg';
+import "../Styles/Navbar.css"
+
+const navbar = () =>  
+{
+    return (
+        <Navbar className="navbar navbar-expand navbar-dark" style={{backgroundColor: "#925bc9"}}> 
+        <Nav.Link as={Link} to="/" className="navbar-brand">
+            <img src={logo} width="40" height="40" className="d-inline-block align-top" alt="logo"/>
+            <span className="h2" style={{ fontFamily:'Lilita One', marginLeft: "5px" }}>Project <span style={{ color:"#ffd91d" }}>Happy</span></span>
+        </Nav.Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav" style={{fontStyle: 'italic'}}>
+            <ul className="navbar-nav mr-auto">
+                <Nav.Link as={Link} to="/" class="nav-item ml-3" style={{marginLeft: "15px"}}>Random</Nav.Link>
+
+                <NavDropdown title="Categories" style={{marginLeft: "15px"}}>
+                    <NavDropdown.Item as={Link} to="/">Cats</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Quizzes</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Minigames</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Jokes</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Quotes</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/">Daily Pool</NavDropdown.Item>
+                </NavDropdown>
+
+                <Nav.Link href="#" style={{marginLeft: "15px"}}>Favourites</Nav.Link>
+            </ul>
+
+            <ul className="navbar-nav mr-auto" style={{position: 'absolute', right: 20}}>
+                <Nav.Link href="#" class="nav-item mr-3">Login</Nav.Link>
+                <Nav.Link href="#" class="nav-item mr-3" style={{marginRight: "10px"}}>Register</Nav.Link>
+            </ul>
+        </div>
+        </Navbar>
+    );
+}
+
+export default navbar;
