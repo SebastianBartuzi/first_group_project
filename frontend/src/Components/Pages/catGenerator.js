d7c4ee04304303c96113022f22902745f9f0b4cf
Krysia PC <>
2021-02-12 12:57:37 +0000

    Added button to cat generator

--- frontend/src/Components/Pages/catGenerator.js
+++ frontend/src/Components/Pages/catGenerator.js
@@ -1,12 +1,19 @@
 import axios from 'axios'
 import {useState} from 'react'
 import CatImage from '../catImage.js';
+import "../../Styles/form.css";
 
 const CatGenerator = () =>{
 
+    function refreshPage() {
+        window.location.reload(false); 
+    }
+    
+    
     return (
         <div class="content-box">
             <CatImage/>
+            <button onClick = {refreshPage} class="button" style={{marginTop: "1em"}}>Next Cat</button>
         </div>
     )
 }
