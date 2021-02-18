b263f9ed38fba8a393f21ea95eca58e48a852f66
Ben Smith <ben.smith-8@student.manchester.ac.uk>
2021-02-18 10:44:25 +0000

    Changed description

--- frontend/src/Components/Pages/landingPage.js
+++ frontend/src/Components/Pages/landingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () =>{
         <div className="centeredDiv" style={{height: "100%", width: "100%"}}>
             <img src={logoText}></img>
             <h1 className="title">Content To Make You Content</h1>
-            <p>I'm baby cray keytar succulents, kinfolk fam you probably haven't heard of them slow-carb waistcoat plaid umami banh mi lyft vaporware. Pug synth jean shorts deep v actually polaroid cardigan tofu roof party aesthetic. XOXO ennui 3 wolf moon, put a bird on it salvia trust fund health goth mumblecore tote bag live-edge sustainable banh mi sartorial austin knausgaard. Air plant tumblr lumbersexual forage you probably haven't heard of them irony. Edison bulb vape poke gentrify fixie portland. Tacos offal +1 yuccie freegan blog four dollar toast sriracha coloring book disrupt tote bag.</p>
+            <p>Are you feeling bored? Are you feeling anxious? Looking for a place to get your mind off things? If your answer is yes, then you are on the right page. Project Happy gives you access to different categories of entertaining content such as minigames, quizzes, polls, jokes, riddles, and many more.  Not sure where to start? Click our random button generator to be taken to a randomly selected page!</p>
         </div>
     )
 }
